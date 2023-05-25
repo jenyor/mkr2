423a500f06d93ba9c5a88b5ceef7aa483eb7a22a
jenyor <yurii.shymon@knu.ua>
2023-05-25 10:00:26 +0300

    Add 2 views: main, category_list

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from recipe import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('categories', views.category_list, name='category_list'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,18 @@
+from django.shortcuts import render
+from .models import Recipe, Category
+from django.db import models
 
+def main(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:5]
+    context = {'latest_recipes': latest_recipes}
+    return render(request, 'main.html', context)
+
+def category_list(request):
+    categories = Category.objects.all()
+    category_recipe_counts = {}
+
+    for category in categories:
+        recipe_count = Recipe.objects.filter(category=category).count()
+        category_recipe_counts[category] = recipe_count
+
+    return render(request, 'category_list.html', {'category_recipe_counts': category_recipe_counts})
